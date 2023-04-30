a03d3ff078bea3ff6849925583b3a31809341845
unknown <yakovlev.drezna.stydy@mail.ru>
2023-04-30 13:33:51 +0300

    wheater http response

--- practitum.py
+++ practitum.py
@@ -797,3 +797,38 @@ new = user_query.split(' ')
 new_string = '%20'.join(new)
 # ваш код здесь
 print(url+new_string)
+
+# Расшифруйте, какой вопрос задал Яндексу пользователь.
+
+import urllib.parse
+
+
+url = 'https://yandex.ru/search/?text=%D0%BA%D0%B0%D0%BA%20%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE%20%D0%B5%D0%B7%D0%B4%D0%B8%D1%82%D1%8C%20%D0%BD%D0%B0%20%D1%82%D0%B0%D0%BA%D1%81%D0%B8'
+
+# чтобы вычленить текст вопроса
+# разбейте строку по знаку = и возьмите
+# второй элемент получившегося списка
+question = url.split('=') # сохраните его в переменной question
+#print(question[1])
+# напечатайте на экран запрос в расшифрованном виде
+print(urllib.parse.unquote(question[1]))  # ваш код здесь
+
+
+# Запросите погодный сервис http://wttr.in по URL без параметров.
+# А их задайте словарём weather_parameters. Функция get() должна принимать его, как отдельный аргумент params.
+
+import requests
+
+
+url = 'https://wttr.in'  # не изменяйте значение URL
+
+weather_parameters = {
+    '0': '',
+    'T': ''
+    # добавьте параметр запроса `T`, чтобы вернулся чёрно-белый текст
+}
+
+response = requests.get(url, params=weather_parameters)  # передайте параметры в http-запрос
+
+print(response.text)
+
